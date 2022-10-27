636337bc6b4cb6bd82d0e4bee59af689abddfaa2
Chris Khiev <chrissnakhiev@gmail.com>
2022-10-27 14:34:29 -0400

    added button functionality to the create new character

--- public/js/characterLanding.js
+++ public/js/characterLanding.js
@@ -1,8 +1,23 @@
+const creation = async () => {
+    document.location.replace('/characterCreation');
+}
+
+const createBtn = document.querySelector('#createBtn');
+createBtn.addEventListener('click', creation)
+
 const selectEl = document.querySelector('#selectChar');
 selectEl.addEventListener('click', () => {
     document.location.replace('/town/1');
 });
+
 const gotograve = document.querySelector('#visitGrav');
 gotograve.addEventListener('click', () => {
     document.location.replace('/graveyard');
 });
+
+const kill = async (event) => {
+    const res = await fetch(`/api/character/${event.target.dataset.id}`, {
+        method: 'DELETE'
+    });
+    res.ok ? document.location.reload() :alert( await res.json());
+}
