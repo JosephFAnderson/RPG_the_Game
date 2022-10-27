5076cc55ed834efbd9513cbea05c546bdb031be4
Stiicer <diegoandremora@gmail.com>
2022-10-27 11:57:01 -0400

    added charcter routes

---
+++ controllers/api/characterRoutes.js
@@ -0,0 +1,57 @@
+const router = require('express').Router();
+const { Character, User } = require('../../models');
+const withAuth = require("../../utils/auth");
+
+router.post('/', withAuth, async (req, res) => {
+    req.body.user_id = req.session.user_id;
+    try {
+        const newChar = await Character.create(
+                req.body
+        );
+
+res.status(200).json(newChar);
+        
+    }
+        catch(err){
+        res.status(500).json(err)    
+    }
+});
+       
+
+router.put('/:id', async (res, req) =>{
+
+
+
+}
+
+
+
+
+
+) 
+router.delete('/:id',  async (req,res)=> {
+    try{
+        const deleteChar =await Character.destroy({
+            where:{
+                id: req.params.id
+            },
+        });
+        if(!deleteChar){
+            res.status(404).json({message:'No Character found'});
+            return;
+        }
+        res.status(200).json(deleteChar);
+    } catch (err){
+        res.status(500).json(err);
+    }
+});
+
+
+
+
+
+
+
+
+
+module.exports=router
