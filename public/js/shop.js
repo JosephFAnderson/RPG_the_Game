5f5fa3b8f6c9aecded694c1c9bfb4db326c8d6ed
Joseph A <joseph_anderson11@yahoo.com>
2022-10-27 13:41:30 -0400

    update user on item buy

--- public/js/shop.js
+++ public/js/shop.js
@@ -12,3 +12,54 @@ const filterShop = (event, itemType) =>{
     document.getElementById(itemType).style.display = "block";
     event.target.className += " active";
 }
+
+const returnTown = (event) => {
+    document.location.replace(`/town/${event.target.dataset.id}`)
+};
+
+const buyItem = async (event) => {    
+    if(event.target.tagName === "BUTTON"){
+        const price = parseInt(event.target.previousElementSibling.dataset.price);
+        const curGold = parseInt(document.querySelector('#curGold').dataset.gold);
+        const itemId = parseInt(event.target.parentNode.dataset.id);
+        const itemType = event.target.parentNode.dataset.type;
+        const char_id = document.querySelector('#townBtn').dataset.id;
+
+        if(curGold < price){
+            console.log("Need more money");
+            return;
+        }
+
+        const newGold = curGold - price;
+        let newChar;
+
+        if(itemType === 'weapon'){
+            newChar = {
+                gold: newGold,
+                weapon_id: itemId
+            }
+        }else{
+            newChar = {
+                gold: newGold,
+                armor_id: itemId
+            }
+        }
+
+        const res = await fetch(`/api/character/${char_id}`,{
+            method: 'PUT',
+            body: JSON.stringify(newChar),
+            headers: { 'Content-type': 'application/json' }
+        })
+
+        if(res.ok){
+            document.querySelector('#curGold').textContent = `You have ${newGold} gold`
+            document.querySelector('#curGold').dataset.gold = newGold;
+        }
+    }
+}
+
+const townEl = document.querySelector('#townBtn');
+townEl.addEventListener('click', returnTown)
+
+const shopEl = document.querySelector('#shop');
+shopEl.addEventListener('click', buyItem)
