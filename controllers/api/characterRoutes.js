45b46d55d29d7b59f863a5277139022829fe3c2c
Stiicer <diegoandremora@gmail.com>
2022-10-27 13:23:03 -0400

    added put route to character routes

--- controllers/api/characterRoutes.js
+++ controllers/api/characterRoutes.js
@@ -2,46 +2,50 @@ const router = require('express').Router();
 const { Character, User } = require('../../models');
 const withAuth = require("../../utils/auth");
 
-router.post('/', withAuth, async (req, res) => {
+router.post('/', async (req, res) => {
     req.body.user_id = req.session.user_id;
     try {
         const newChar = await Character.create(
-                req.body
+            req.body
         );
 
-res.status(200).json(newChar);
-        
+        res.status(200).json(newChar);
+
     }
-        catch(err){
-        res.status(500).json(err)    
+    catch (err) {
+        res.status(500).json(err)
     }
 });
-       
-
-router.put('/:id', async (res, req) =>{
 
 
+router.put('/:id', async (req, res) => {
+    req.body.user_id = req.session.user_id;
 
-}
+    Character.update(
+        req.body,
+        {
+            where: { id: req.params.id, }
+        },
+    )
+        .then((updatedCharacter) => {
+            res.json(updatedCharacter)
+        })
+        .catch((err) => res.json(err))
+});
 
-
-
-
-
-) 
-router.delete('/:id',  async (req,res)=> {
-    try{
-        const deleteChar =await Character.destroy({
-            where:{
+router.delete('/:id', async (req, res) => {
+    try {
+        const deleteChar = await Character.destroy({
+            where: {
                 id: req.params.id
             },
         });
-        if(!deleteChar){
-            res.status(404).json({message:'No Character found'});
+        if (!deleteChar) {
+            res.status(404).json({ message: 'No Character found' });
             return;
         }
         res.status(200).json(deleteChar);
-    } catch (err){
+    } catch (err) {
         res.status(500).json(err);
     }
 });
@@ -54,4 +58,4 @@ router.delete('/:id',  async (req,res)=> {
 
 
 
-module.exports=router
+module.exports = router
