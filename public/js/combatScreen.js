f19a9b3f36587230579cec653220782dc9e62a35
Joseph A <joseph_anderson11@yahoo.com>
2022-10-28 19:38:35 -0400

    attack button

--- public/js/combatScreen.js
+++ public/js/combatScreen.js
@@ -1,8 +1,6 @@
 let character;
 let monster;
-let order;
-
-
+const initative = [];
 
 const roll = (input) => {
     try{
@@ -18,7 +16,6 @@ const roll = (input) => {
         let sum = 0;
         for(let i = 1; i <= dice[0]; i++){
             const roll = Math.floor(Math.random()*dice[1])+1
-            console.log(`rolled a ${roll}`);
             sum += roll;
         }
         return sum + mod;
@@ -26,34 +23,86 @@ const roll = (input) => {
     }   
 }
 
+const attack = () => {
+    for(let i = 0; i < 2; i++){
+        if(initative[i].name === character.name){
+            const damage = roll(`${character.weapon_damage}+${character.strength}`);
+            console.log(`${character.name} attacks ${monster.name} for ${damage}`);
+            let damageTaken = damage - monster.defense;
+            if(damageTaken < 0){
+                damageTaken = 0
+            }
+
+            console.log(`${monster.name} takes ${damageTaken} after armor`);
+
+            monster.health -= damageTaken;
+            if(monster.health < 1){
+                console.log(`${monster.name} has fallen\n`)
+            }else{
+                console.log(`${monster.name} has ${monster.health} health left\n`)
+            }
+       
+        } else{
+            const damage = monster.strength;
+            console.log(`${monster.name} attacks ${character.name} for ${damage}`);
+            let damageTaken = damage - character.defense;
+            if(damageTaken < 0){
+                damageTaken = 0
+            }
+
+            console.log(`${character.name} takes ${damageTaken} after armor`);
+            character.health -= damageTaken;
+            if(character.health < 1){
+                console.log(`${character.name} has fallen\n`)
+            }else{
+                console.log(`${character.name} has ${character.health} health left\n`)
+            }
+            
+        }
+    }
+};
+
 const getReady = async () => {
     const charEl = document.querySelector("#player");
     const monEl = document.querySelector("#monster");
 
     const charData = await fetch(`/api/character/${charEl.dataset.id}`).then(res => res.json()).then(data => data);
     character = {
+        name: charData.name,
         strength: charData.strength,
         defense: charData.defense + charData.armor.defense,
         vitality: charData.vitality,
         weapon_damage: charData.weapon.damage,
         experience: charData.experience,
-        gold: charData.gold
+        gold: charData.gold,
+        health: charData.vitality*4
     }
+
+    initative.push(character);
     
     const monData = await fetch(`/api/monster/${monEl.dataset.id}`).then(res => res.json()).then(data => data)
     monster = {
+        name: monData.name,
         strength: monData.strength,
         defense: monData.defense,
         vitality: monData.vitality,
         exp_worth: monData.experience_given,
-        gold_drop: monData.gold_dropped
+        gold_drop: monData.gold_dropped,
+        health: monData.vitality*4
     }
 
-    order = [Math.floor(Math.random() * 2)]
-    order[0] === 1 ? order.push(0) : order.push(1);
+    const order = Math.floor(Math.random() * 2)
+    order === 1 ? initative.push(monster) : initative.unshift(monster);
+
+    console.log(character);
+    console.log(monster);
+    console.log(initative);
 }
 
 getReady();
+
+const atkBtn = document.querySelector('#attackBtn');
+atkBtn.addEventListener('click', attack)
 // const atkBtn = document.querySelector('#attackBtn');
 // const fleeBtn= document.querySelector('#fleeBtn');
 // const monster = require('../Monster');
