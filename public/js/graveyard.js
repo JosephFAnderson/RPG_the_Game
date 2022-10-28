59f12766524f4e540edc8a1daa2c98de70e74852
Joseph A <joseph_anderson11@yahoo.com>
2022-10-28 09:49:07 -0400

    leaf animations

--- public/js/graveyard.js
+++ public/js/graveyard.js
@@ -1,4 +1,18 @@
+// const anime = require('animejs');
+
 const gotochars = document.querySelector('#return');
 gotochars.addEventListener('click', () => {
     document.location.replace('/characterlanding');
+});
+
+var path = anime.path('#leafPath path');
+
+anime({
+  targets: '#leaf, #leaf2, #leaf3',
+  translateX: path('x'),
+  translateY: path('y'),
+  rotate: path('angle'),
+  easing: 'linear',
+  duration: 15000,
+  loop: true
 });
